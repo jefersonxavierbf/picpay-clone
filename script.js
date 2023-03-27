6e21677b2f13b14c0cba113870b1e8846f8fa0e4
Jeferson Xavier <jefersonxavierbf@gmail.com>
2023-03-27 08:53:07 -0300

    finalização da barra de menu

---
+++ script.js
@@ -0,0 +1,50 @@
+const body = document.querySelector('body');
+const menu = document.querySelector('.menu');
+const menuBtns = document.querySelectorAll('.menu-btns');
+const subMenu = document.querySelectorAll('.sub-menu');
+const fechasMenuBtns = document.querySelectorAll('.fechas-menu-btns');
+const acessarBlog = document.querySelector('.acessar-blog')
+const abrirConta = document.querySelector('.abrir-conta')
+const btnMobile = document.querySelector('.btn-mobile');
+const fecharBtnMobile = document.querySelector('.fechar-btn-mobile');
+
+menuBtns.forEach((btn) => {
+    btn.addEventListener('click', (elemento) => {
+        const elementoSelecionado = elemento.currentTarget;
+        for(let i = 0; i < menuBtns.length; i++) {
+            if(elementoSelecionado === menuBtns[i]) {
+                subMenu[i].classList.toggle('ativa-sub-menu');
+                menuBtns[i].classList.toggle('ativa-cor-btns-menu');
+                fechasMenuBtns[i].classList.toggle('gira-fecha-btn-menu');
+            }
+            if(elementoSelecionado !== menuBtns[i]) {
+                subMenu[i].classList.remove('ativa-sub-menu');
+                menuBtns[i].classList.remove('ativa-cor-btns-menu');
+                fechasMenuBtns[i].classList.remove('gira-fecha-btn-menu');
+            }
+        }
+    })
+});
+
+btnMobile.addEventListener('click', () => {
+    if (btnMobile.classList.contains('ativar')) {
+        btnMobile.classList.remove('ativar');
+        body.classList.add('removerRolagem');
+        fecharBtnMobile.classList.add('ativar');
+        menu.classList.add('ativar-flex');
+        acessarBlog.classList.add('ativar')
+        abrirConta.classList.add('ativar');
+    }
+});
+
+fecharBtnMobile.addEventListener('click', () => {
+    if (fecharBtnMobile.classList.contains('ativar')) {
+        fecharBtnMobile.classList.remove('ativar');
+        body.classList.remove('removerRolagem');
+        btnMobile.classList.add('ativar');
+        menu.classList.remove('ativar-flex');
+        abrirConta.classList.remove('ativar');
+        abrirConta.classList.remove('ativar');
+    }
+});
+
